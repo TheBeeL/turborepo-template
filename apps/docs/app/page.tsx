17a012d5823fe5a249e649866617eee6b043b18a
Cyan0n <cyan0n.dev@gmail.com>
2023-08-31 12:17:52 +0200

    Connected ui package tailwind into apps

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,4 +1,4 @@
-import { Button, Header } from "ui";
+import { Button, Header } from "@acme/ui";
 
 export default function Page(): JSX.Element {
   return (
--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  transpilePackages: ["ui"],
+  transpilePackages: ["@acme/ui"],
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
